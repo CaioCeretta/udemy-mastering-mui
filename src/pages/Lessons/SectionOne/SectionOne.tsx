5b0609f4d602f5c277bb33c52cb92de8d19f2fae
Caio <caio.soares@vcimentos.com>
2026-04-19 23:36:37 -0300

    chore: First lesson

--- src/pages/Lessons/SectionOne/SectionOne.tsx
+++ src/pages/Lessons/SectionOne/SectionOne.tsx
@@ -1,6 +1,29 @@
+import { Button, Box } from "@mui/material"
+
+const MySpecialBox = (props: any) => {
+  return (
+    <Box>
+      Label: {props.label}
+      {props.children}
+    </Box>
+  )
+  
+}
 
 export default function Section1Lessons() {
   return (
-    <div>Section1Lessons</div>
+    <div>
+      {/* <Button
+        variant="contained"
+        onClick={() => console.log("Clicked")}
+        disableElevation
+        component="h1"
+        sx={{ bgcolor: 'green' }}>
+          My Button
+        </Button> */}
+        <MySpecialBox label="Custom Element Example">
+          <Button variant="contained">My Box</Button>
+        </MySpecialBox>
+    </div>
   )
 }
